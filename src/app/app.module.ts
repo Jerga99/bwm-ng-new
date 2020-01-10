222ba8568f1606b142478883ed78450e4a33dea4
Filip Jerga <jerga99@gmail.com>
2020-01-10 17:26:36 +0100

    2 Folder Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// root module
 @NgModule({
   declarations: [
     AppComponent
