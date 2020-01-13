0122125e027ffb5a7e90fedf4f3084450aef5a19
Filip Jerga <jerga99@gmail.com>
2020-01-13 18:55:53 +0100

    18 Interface and Generic

--- src/app/rental/rental-listing/rental-listing.component.ts
+++ src/app/rental/rental-listing/rental-listing.component.ts
@@ -3,20 +3,36 @@ import { Component, OnInit } from '@angular/core';
 import { Rental } from '../shared/rental.model';
 import { RentalService } from '../shared/rental.service';
 
+import { IRental, AppStorage } from '../shared/rental.interface';
+
 // TODO: Interfaces in next lecture
 @Component({
   selector: 'bwm-rental-listing',
   templateUrl: './rental-listing.component.html'
 })
-export class RentalListingComponent implements OnInit{
-  rentals: Rental[] = []
+export class RentalListingComponent implements OnInit, IRental{
+  rentals: Rental[] = [];
+  someData = 'Some Data1';
+  isLoaded = false;
 
   constructor(private rentalService: RentalService){}
 
   ngOnInit() {
+    const appStorage = new AppStorage<string>();
+
+    appStorage.addItem("23");
+    appStorage.addItem("2343433");
+
+    const item = appStorage.getItem(0);
+    const items = appStorage.displayItems();
+
     this.rentalService.getRentals()
       .subscribe((rentals: Rental[]) => {
         this.rentals = rentals;
     });
+  }
+
+  implementMe() {
+    return '';
   }
 }
