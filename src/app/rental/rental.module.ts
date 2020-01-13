aa5288a9861265bb380769f31c2c5c13c276c6e6
Filip Jerga <jerga99@gmail.com>
2020-01-13 11:38:19 +0100

    16 Iterate Rentals

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -1,6 +1,7 @@
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 import { RentalDetailComponent } from './rental-detail/rental-detail.component';
 import { RentalListingComponent } from './rental-listing/rental-listing.component';
@@ -24,7 +25,8 @@ const routes: Routes = [
     RentalComponent
   ],
   imports: [
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    CommonModule
   ]
 })
 export class RentalModule {}
