c235f59597928a9140aecb459219ee6abdb447ca
Filip Jerga <jerga99@gmail.com>
2020-01-13 11:31:15 +0100

    15 Define Rental Data

--- src/app/rental/rental-listing/rental-listing.component.ts
+++ src/app/rental/rental-listing/rental-listing.component.ts
@@ -1,11 +1,64 @@
 
 import { Component } from '@angular/core';
-
+import { Rental } from '../shared/rental.model';
 
 @Component({
   selector: 'bwm-rental-listing',
   templateUrl: './rental-listing.component.html'
 })
 export class RentalListingComponent {
-
+  rentals: Rental[] = [
+   {
+     _id: '1',
+     title: 'Central Apartment',
+     city: 'New York',
+     street: 'Times Square',
+     category: 'apartment',
+     image: 'http://via.placeholder.com/350x250',
+     numOfRooms: 3,
+     description: 'Very nice apartment',
+     dailyPrice: 34,
+     shared: false,
+     createdAt: '1/1/2020'
+   },
+   {
+    _id: "2",
+    title: "Central Apartment 2",
+    city: "San Francisco",
+    street: "Main street",
+    category: "condo",
+    image: "http://via.placeholder.com/350x250",
+    numOfRooms: 2,
+    description: "Very nice apartment",
+    dailyPrice: 12,
+    shared: true,
+    createdAt: "24/12/2017"
+  },
+  {
+    _id: "3",
+    title: "Central Apartment 3",
+    city: "Bratislava",
+    street: "Hlavna",
+    category: "condo",
+    image: "http://via.placeholder.com/350x250",
+    numOfRooms: 2,
+    description: "Very nice apartment",
+    dailyPrice: 334,
+    shared: true,
+    createdAt: "24/12/2017"
+  },
+  {
+    _id: "4",
+    title: "Central Apartment 4",
+    city: "Berlin",
+    street: "Haupt strasse",
+    category: "house",
+    image: "http://via.placeholder.com/350x250",
+    numOfRooms: 9,
+    description: "Very nice apartment",
+    dailyPrice: 33,
+    shared: true,
+    createdAt: "24/12/2017"
+  }
+  ]
 }
---
+++ src/app/rental/shared/rental.model.ts
@@ -0,0 +1,16 @@
+
+
+
+export class Rental {
+  _id: string; // unique identifier as it will be stored in DB
+  title: string; // Some nice place in LA
+  city: string; // Los Angeles
+  street: string; // Main Street
+  category: string; // apartment
+  image: string; // https://someurlOfImage.png
+  numOfRooms: number; // 5
+  description: string; // Some nice place near a beach.
+  dailyPrice: number; // 127
+  shared: boolean; // true || false
+  createdAt: string; // 23/12/2020 11:11:11
+}
