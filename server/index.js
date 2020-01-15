f1ee4f61b3eadf9a256f3cb5a3ca371d3cde96f4
Filip Jerga <jerga99@gmail.com>
2020-01-15 14:26:57 +0100

    32 Endpoints

--- server/index.js
+++ server/index.js
@@ -4,6 +4,33 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const rentals = [
+  {
+    _id: '2137129312',
+    city: 'New York',
+    title: 'Very nice place'
+  },
+  {
+    _id: 'asjkdajdasnda',
+    city: 'Berlin',
+    title: 'Very nice place as well!'
+  },
+]
+
+app.get('/api/v1/rentals', (req, res) => {
+  return res.json(rentals);
+})
+
+app.get('/api/v1/rentals/:rentalId', (req, res) => {
+  const { rentalId } = req.params;
+  const rental = rentals.find(r => r._id === rentalId);
+
+  return res.json(rental)
+})
+
+
+
+
 app.listen(PORT, () => {
   console.log('Server is listening on port: ', PORT);
 })
