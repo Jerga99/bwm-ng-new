a4d01f8a17730e0beacb343ddfa9d835c34afefb
Filip Jerga <jerga99@gmail.com>
2020-01-15 18:57:44 +0100

    41 Fake DB

--- server/controllers/rentals.js
+++ server/controllers/rentals.js
@@ -25,16 +25,7 @@ exports.getRentalById = (req, res) => {
 
 exports.createRental = (req, res) => {
   const rentalData = req.body;
-  // const newRental = new Rental(rentalData);
-
-  // newRental.save((error, createdRental) => {
-    // if (error) {
-    //   return res.status(422).send({errors: [{title: 'Rental Error!', message: 'Cannot post rental data!'}]})
-    // }
-
-    // return res.json({message: `Rental with id: ${createdRental._id} was added!`});
-  // })
-
+  
   Rental.create(rentalData, (error, createdRental) => {
     if (error) {
       return res.status(422).send({errors: [{title: 'Rental Error!', message: 'Cannot post rental data!'}]})
