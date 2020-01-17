44a3571991e6e7f15487bf06545a357be6e3598b
Filip Jerga <jerga99@gmail.com>
2020-01-17 19:16:45 +0100

    52 Custom directive validator

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ForbiddenEmailDirective } from '../shared/validators/forbidden-email.directive';
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ForbiddenEmailDirective
   ],
   imports: [
     CommonModule,
---
+++ src/app/shared/validators/forbidden-email.directive.ts
@@ -0,0 +1,27 @@
+
+
+import { Directive, Input } from '@angular/core';
+import { Validator, NG_VALIDATORS, AbstractControl } from '@angular/forms';
+import { forbiddenEmailValidator } from './functions';
+
+// TODO: Explain providers!
+@Directive({
+  selector: '[forbiddenEmail]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: ForbiddenEmailDirective,
+      multi: true
+    }
+  ]
+})
+export class ForbiddenEmailDirective implements Validator {
+
+  @Input('forbiddenEmail') forbiddenEmail: string;
+
+  validate(control: AbstractControl): {[key: string]: any} | null {
+    debugger
+    return this.forbiddenEmail ? 
+      forbiddenEmailValidator(this.forbiddenEmail)(control) : null
+  }
+}
