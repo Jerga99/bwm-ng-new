40e1ef537db4a730825ddc807d3e68e7ad3243b1
Filip Jerga <jerga99@gmail.com>
2020-01-20 16:20:36 +0100

    57 Register User

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,5 @@
 
-
+const User = require('../models/user');
 
 exports.login = (req, res) => {
 
@@ -8,6 +8,32 @@ exports.login = (req, res) => {
 
 
 exports.register = (req, res) => {
+  const { username, email, password, passwordConfirmation } = req.body;
 
-  return res.json({message: 'Registering!'});
+  if (!password || !email) {
+    return res.status(422).send({errors: [{title: 'Missing Data', detail: 'Email or password is missing!'}]});
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.status(422).send({errors: [{title: 'Invalid password', detail: 'Password is not maching confirmation password!'}]});
+  }
+
+  User.findOne({email}, (error, existingUser) => {
+    if (error) {
+      return res.status(422).send({errors: [{title: 'DB Error', detail: 'Oooops, something went wrong!'}]});
+    }
+
+    if (existingUser) {
+      return res.status(422).send({errors: [{title: 'Invalid Email', detail: 'User with provided email already exists!'}]});
+    }
+
+    const user = new User({username, email, password});
+    user.save((error) => {
+      if (error) {
+        return res.status(422).send({errors: [{title: 'DB Error', detail: 'Oooops, something went wrong!'}]});
+      }
+
+      return res.json({status: 'registered'});
+    })
+  })
 }
