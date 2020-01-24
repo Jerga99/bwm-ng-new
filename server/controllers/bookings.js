9364b4e19c472a2d3d1400e57d14c2a6f0101f5a
Filip Jerga <jerga99@gmail.com>
2020-01-24 18:48:29 +0100

    102 Check Booking Validity

--- server/controllers/bookings.js
+++ server/controllers/bookings.js
@@ -1,10 +1,17 @@
 
 const Booking = require('../models/booking');
-
+const moment = require('moment');
 
 exports.createBooking = (req, res) => {
   const bookingData = req.body;
   const booking = new Booking({...bookingData, user: res.locals.user});
+
+  if (!checkIfBookingDatesAreValid(booking)) {
+    return res
+      .sendApiError(
+        { title: 'Invalid Booking', 
+          detail: 'Dates are invalid!'});
+  }
   
   Booking.find({rental: booking.rental}, (error, rentalBookings) => {
     if (error) { return res.mongoError(error); }
@@ -17,12 +24,45 @@ exports.createBooking = (req, res) => {
         return res.json({startAt: savedBooking.startAt, endAt: savedBooking.endAt})
       })
     } else {
-      return res.json({message: 'Booking is NOT created!'});
+      return res
+        .sendApiError(
+          { title: 'Invalid Booking', 
+            detail: 'Choosen dates are already taken!'});
     }
   })
 }
 
-// TODO: Provide actual implementation
-function checkIfBookingIsValid(booking, rentalBookings) {
-  return true;
+function checkIfBookingDatesAreValid(booking) {
+  let isValid = true;
+
+  if (!booking.startAt || !booking.endAt) {
+    isValid = false;
+  }
+
+  if (moment(booking.startAt) > moment(booking.endAt)) {
+    isValid = false;
+  }
+
+  return isValid;
+}
+
+function checkIfBookingIsValid(pendingBooking, rentalBookings) {
+  let isValid = true;
+
+  if (rentalBookings && rentalBookings.length > 0) {
+
+    isValid = rentalBookings.every(booking => {
+      const pendingStart = moment(pendingBooking.startAt);
+      const pendingEnd = moment(pendingBooking.endAt);
+
+      const bookingStart = moment(booking.startAt);
+      const bookingEnd = moment(booking.endAt);
+
+      return ((bookingStart < pendingStart) && bookingEnd < pendingStart || 
+              (pendingEnd < bookingEnd && pendingEnd < bookingStart));
+
+    })
+  }
+
+  return isValid
 }
