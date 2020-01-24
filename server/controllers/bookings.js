dcbcd3b9d90a16c97a0d0ad2c435c2edc8b2f4b4
Filip Jerga <jerga99@gmail.com>
2020-01-24 17:10:51 +0100

    101 Create booking

--- server/controllers/bookings.js
+++ server/controllers/bookings.js
@@ -4,8 +4,25 @@ const Booking = require('../models/booking');
 
 exports.createBooking = (req, res) => {
   const bookingData = req.body;
+  const booking = new Booking({...bookingData, user: res.locals.user});
+  
+  Booking.find({rental: booking.rental}, (error, rentalBookings) => {
+    if (error) { return res.mongoError(error); }
 
-  
-  
-  return res.json({message: 'Booking is created!'});
+    const isValid = checkIfBookingIsValid(booking, rentalBookings);
+    if (isValid) {
+      booking.save((error, savedBooking) => {
+        if (error) { return res.mongoError(error); }
+
+        return res.json({startAt: savedBooking.startAt, endAt: savedBooking.endAt})
+      })
+    } else {
+      return res.json({message: 'Booking is NOT created!'});
+    }
+  })
+}
+
+// TODO: Provide actual implementation
+function checkIfBookingIsValid(booking, rentalBookings) {
+  return true;
 }
