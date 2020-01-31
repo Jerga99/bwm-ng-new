f36a61b18876edb836605dce0e5d36f8adc61a62
Filip Jerga <jerga99@gmail.com>
2020-01-31 12:11:21 +0100

    150 Multer Init

--- server/routes/image-upload.js
+++ server/routes/image-upload.js
@@ -2,10 +2,37 @@
 
 const express = require('express');
 const router = express.Router();
+
 const { onlyAuthUser } = require('../controllers/users');
 
-router.post('', onlyAuthUser, (req, res) => {
-  return res.json({message: 'Uploading File...'})
+const upload = require('../services/multer');
+const singleUpload = upload.single('image');
+
+const singleUploadCtrl = (req, res, next) => {
+  singleUpload(req, res, (error) => {
+    if (error) {
+      return res.sendApiError(
+        { title: 'Upload Error', 
+          detail:  error.message});
+    }
+
+    next();
+  })
+}
+
+router.post('', onlyAuthUser, singleUploadCtrl, (req, res) => {
+
+  try {
+    if (!req.file) { throw new Error('Image is not presented!');}
+    console.log(req.file);
+    return res.json({message: 'Uploading File...'})
+  } catch(error) {
+    return res.sendApiError(
+      { title: 'Upload Error', 
+        detail:  error.message});
+  }
+
+  
 });
 
 
