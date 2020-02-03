e6f1f3aa5a208dda1416472cec80b222059b651f
Filip Jerga <jerga99@gmail.com>
2020-02-03 08:57:00 +0100

    158 Adds file reader and show image preview

--- src/app/shared/modules/image-upload/image-upload.component.ts
+++ src/app/shared/modules/image-upload/image-upload.component.ts
@@ -1,21 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+
+class ImageSnippet {
+  src: string;
+  file: File;
+  status: string;
+
+  constructor(file?: File) {
+    this.file = file;
+    this.status = 'INIT';
+  }
+}
+
 
 @Component({
   selector: 'bwm-image-upload',
   templateUrl: './image-upload.component.html',
   styleUrls: ['./image-upload.component.scss']
 })
-export class ImageUploadComponent implements OnInit {
+export class ImageUploadComponent implements OnInit, OnDestroy {
+
+  selectedImage: ImageSnippet;
+
+  private fileReader = new FileReader();
 
   constructor() { }
 
   ngOnInit() {
+    this.listenToFileLoading();
+  }
+
+  ngOnDestroy() {
+    this.removeFileLoadListener();
   }
 
   onImageLoad(event: any) {
-    debugger
     const file = event.target.files[0];
-    console.log(file);
+    
+    this.selectedImage = new ImageSnippet(file);
+    // this will fire 'load' event
+    this.fileReader.readAsDataURL(file);
   }
 
+  private handleImageLoad = (event: any) => {
+    const { result } = event.target;
+    this.selectedImage.src = result;
+    this.selectedImage.status = 'LOADED';
+  }
+
+  private listenToFileLoading() {
+    this.fileReader.addEventListener('load', this.handleImageLoad);
+  }
+
+  private removeFileLoadListener() {
+    this.fileReader.removeEventListener('load', this.handleImageLoad);
+  }
 }
